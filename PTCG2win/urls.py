1dd28cc903d3001e753e8eece1aaf09ea6f46547
Lagyu <sasaki.y@ruri.waseda.jp>
2018-05-18 21:28:06 +0900

    collected root redirection

--- PTCG2win/urls.py
+++ PTCG2win/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path("decks/", include("decks.urls")),
     path('admin/', admin.site.urls),
-    url(r'^$', include('decks.urls')),
-    url(r'', include('decks.urls')),
+    path(r'^$', include('decks.urls')),
+    path(r'', include('decks.urls')),
 ]
