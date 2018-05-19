e97751bf8317f7ec15827fe713057669ea432e51
Lagyu <sasaki.y@ruri.waseda.jp>
2018-05-19 17:45:02 +0900

    Defined cards

--- decks/models.py
+++ decks/models.py
@@ -28,6 +28,7 @@ class Country(models.Model):
 class Expansion(models.Model):
     name = models.CharField(max_length=100)
     pub_date = models.DateField()
+    code = models.CharField(max_length=10)
 
     def __str__(self):
         return self.name
@@ -58,6 +59,15 @@ class Series(models.Model):
         return self.name
 
 
+class PokemonSpecies(models.Model):
+    name = models.CharField(max_length=20)
+    pokedex_number = models.IntegerField()
+    region = models.CharField(default="")
+
+    def __str__(self):
+        return self.name
+
+
 class Card(models.Model):
     artist = models.CharField(max_length=50)
     expansion = models.ForeignKey(Expansion, on_delete=models.PROTECT)  # setと対応
@@ -68,14 +78,7 @@ class Card(models.Model):
     number = models.IntegerField()
     rarity = models.ForeignKey(Rarity, on_delete=models.PROTECT)
     series = models.ForeignKey(Series, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return self.name
-
-
-class Ability(models.Model):
-    name = models.CharField(max_length=50)
-    text = models.CharField(max_length=500)
+    is_prism_star = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -88,25 +91,85 @@ class Type(models.Model):
         return self.name
 
 
-class WeaknessType(models.Model):
+class Weakness(models.Model):
     name = models.CharField(max_length=50)
-    text = models.CharField(max_length=20)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    effect = models.CharField(max_length=10)  # e.g. "*2" "+30"
 
     def __str__(self):
         return self.name
 
 
 class Pokemon(Card):
+    name = models.CharField(max_length=20)
     hp = models.IntegerField()
-    national_pokedex_number = models.IntegerField()
     retreat_cost = models.IntegerField()
-    weakness = models.ForeignKey(Type, on_delete=models.PROTECT)
-    weakness_type = models.ForeignKey(WeaknessType, on_delete=models.PROTECT)  # I use "type" here in general meaning.
+    weakness = models.ForeignKey(Weakness, on_delete=models.PROTECT)
+    species = models.ForeignKey(PokemonSpecies, on_delete=models.PROTECT)
 
 
+class BasicPokemon(Pokemon):
+    pass
 
 
-class Move(models.Model):
+class StageOnePokemon(Pokemon):
+    evolves_from = models.CharField(max_length=20)
+
+
+class StageTwoPokemon(Pokemon):
+    evolves_from = models.CharField(max_length=20)
+
+
+class BreakPokemon(Pokemon):
+    evolves_from = models.CharField(max_length=20)
+
+
+class EXPokemon(Pokemon):
+    text = models.CharField(max_length=50)
+
+
+class MegaEXPokemon(EXPokemon):
+    evolves_from = models.CharField(max_length=20)
+
+
+class GXBasicPokemon(BasicPokemon):
+    text = models.CharField(max_length=50)
+
+
+class GXStageOnePokemon(StageOnePokemon):
+    text = models.CharField(max_length=50)
+
+
+class GXStageTwoPokemon(StageTwoPokemon):
+    text = models.CharField(max_length=50)
+
+
+class Trainers(Card):
+    text = models.CharField(max_length=500)
+
+
+class Stadium(Trainers):
+    pass
+
+
+class Item(Trainers):
+    pass
+
+
+class Supporter(Trainers):
+    pass
+
+
+class Ability(models.Model):
+    name = models.CharField(max_length=50)
+    text = models.CharField(max_length=500)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Attack(models.Model):
     name = models.CharField(max_length=30)
     damage = models.IntegerField(default=0)
     text = models.CharField(max_length=200)
