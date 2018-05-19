4bd51c40fc79058772bf4778793be73ff04ec688
lagyu <lagyu@live.jp>
2018-05-19 11:06:49 +0900

    Created some views

--- decks/models.py
+++ decks/models.py
@@ -44,15 +44,66 @@ class Regulation(models.Model):
         return self.name
 
 
-class Card(models.Model):
-    expansion = models.ForeignKey(Expansion, on_delete=models.PROTECT)
-    id_in_expansion = models.CharField(max_length=30)
-    name = models.CharField(max_length=100)
-    name_j = models.CharField(max_length=100)
-    hp = models.IntegerField()
+class Rarity(models.Model):
+    name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
+
+
+class Series(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Card(models.Model):
+    artist = models.CharField(max_length=50)
+    expansion = models.ForeignKey(Expansion, on_delete=models.PROTECT)  # setと対応
+    expansion_code = models.CharField(max_length=10)
+    global_id = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
+    name_j = models.CharField(max_length=100)
+    number = models.IntegerField()
+    rarity = models.ForeignKey(Rarity, on_delete=models.PROTECT)
+    series = models.ForeignKey(Series, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+
+class Ability(models.Model):
+    name = models.CharField(max_length=50)
+    text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class WeaknessType(models.Model):
+    name = models.CharField(max_length=50)
+    text = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Pokemon(Card):
+    hp = models.IntegerField()
+    national_pokedex_number = models.IntegerField()
+    retreat_cost = models.IntegerField()
+    weakness = models.ForeignKey(Type, on_delete=models.PROTECT)
+    weakness_type = models.ForeignKey(WeaknessType, on_delete=models.PROTECT)  # I use "type" here in general meaning.
+
+
 
 
 class Move(models.Model):
@@ -68,7 +119,7 @@ class Move(models.Model):
 
 class ArchType(models.Model):
     name = models.CharField(max_length=100)
-    expansion = models.ForeignKey(Regulation, models.PROTECT)
+    expansion = models.ForeignKey(Regulation, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
@@ -80,16 +131,16 @@ class Deck(models.Model):
     mod_date = models.DateTimeField()
     description = models.CharField(max_length=100000)
     owner = models.ForeignKey(User, on_delete=models.PROTECT)
-    arch_type = models.ForeignKey(ArchType, models.PROTECT)
+    arch_type = models.ForeignKey(ArchType, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
 
 
 class CardBundle(models.Model):
-    card = models.ForeignKey(Card, models.PROTECT)
+    card = models.ForeignKey(Card, on_delete=models.PROTECT)
     amount = models.IntegerField(default=1)
-    deck_list = models.ForeignKey(Deck, models.CASCADE)
+    deck_list = models.ForeignKey(Deck, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.card.name
